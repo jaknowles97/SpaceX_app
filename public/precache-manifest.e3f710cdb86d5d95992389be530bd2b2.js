efa478998bec4f13888a5411f5cd50bf930bd9b7
Andrew <jaknowles1997@gmail.com>
2019-12-10 06:41:58 -0500

    prepare for first deploy to heroku

---
+++ public/precache-manifest.e3f710cdb86d5d95992389be530bd2b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2590257b8a2d7da854c08e0f24cd25e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "013a5fe4c2cd3ebffbe3",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2db6a539333e3094a87e",
+    "url": "/static/js/2.1cc60e41.chunk.js"
+  },
+  {
+    "revision": "786089ef6a78275eeee7a67aba60641a",
+    "url": "/static/js/2.1cc60e41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "013a5fe4c2cd3ebffbe3",
+    "url": "/static/js/main.d3fcb342.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "d0336c4fc6962fb85daa7ffcbfbe94d0",
+    "url": "/static/media/logo.d0336c4f.png"
+  }
+]);
